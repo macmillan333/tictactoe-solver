e4e80617c5af3c737f2f770a1f4d371d1f37041e
macmillan333 <30272345+macmillan333@users.noreply.github.com>
2020-07-10 22:32:58 -0700

    Created empty project.

---
+++ TicTacToeSolver/TicTacToeSolver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TicTacToeSolver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
