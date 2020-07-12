c7465275a2b211dbaa41a0f7025e5811ab29a0c5
macmillan333 <30272345+macmillan333@users.noreply.github.com>
2020-07-11 21:53:46 -0700

    Displaying solved game tree.

--- TicTacToeSolver/TicTacToeGuiSolver/MainWindow.xaml.cs
+++ TicTacToeSolver/TicTacToeGuiSolver/MainWindow.xaml.cs
@@ -25,13 +25,124 @@ namespace TicTacToeGuiSolver
             InitializeComponent();
         }
 
+        private List<State> states;
+
         private void solve_button_Click(object sender, RoutedEventArgs e)
         {
-            State root_state = new State() { ID = 0 };
-            root_state.NextStates.Add(new State() { ID = 123 });
-            root_state.NextStates[0].NextStates.Add(new State() { ID = 124 });
-            root_state.NextStates.Add(new State() { ID = 456 });
-            game_tree.Items.Add(root_state);
+            // Initialize all states.
+            states = new List<State>();
+            for (int id = 0; id <= State.max_id; id++)
+            {
+                states.Add(new State(id));
+            }
+
+            // Backfill previous states. Fill next state objects.
+            for (int id = 0; id <= State.max_id; id++)
+            {
+                states[id].NextStates = new List<State>();
+                foreach (int next_id in states[id].next_states)
+                {
+                    states[id].NextStates.Add(states[next_id]);
+                    states[next_id].prev_states.Add(id);
+                }
+            }
+
+            // Solve.
+            Queue<int> queue = new Queue<int>();
+            // Initially, put all terminal states into the queue.
+            for (int id = 0; id <= State.max_id; id++)
+            {
+                if (states[id].outcome != State.Outcome.Undecided)
+                {
+                    queue.Enqueue(id);
+                }
+            }
+            // Start solving.
+            while (queue.Count > 0)
+            {
+                int id = queue.Dequeue();
+                if (states[id].expected_outcome == State.Outcome.Undecided)
+                {
+                    if (states[id].next_states.Count == 0)
+                    {
+                        throw new InvalidOperationException($"State #{id} is not terminal, yet it has no next states.");
+                    }
+                    // Attempt to solve this state.
+                    // First, count next states.
+                    State.Outcome winning_outcome = (states[id].next_player == State.NextPlayer.O) ?
+                        State.Outcome.OWins : State.Outcome.XWins;
+                    State.Outcome losing_outcome = (states[id].next_player == State.NextPlayer.O) ?
+                        State.Outcome.XWins : State.Outcome.OWins;
+                    bool has_undecided_next_state = false;
+                    bool has_winning_next_state = false;
+                    bool has_draw_next_state = false;
+                    foreach (int next_id in states[id].next_states)
+                    {
+                        switch (states[next_id].expected_outcome)
+                        {
+                            case State.Outcome.Undecided:
+                                has_undecided_next_state = true;
+                                break;
+                            case State.Outcome.Draw:
+                                has_draw_next_state = true;
+                                break;
+                            case State.Outcome.OWins:
+                            case State.Outcome.XWins:
+                                if (states[next_id].expected_outcome == winning_outcome)
+                                {
+                                    has_winning_next_state = true;
+                                }
+                                break;
+                        }
+                    }
+
+                    if (has_winning_next_state)
+                    {
+                        // If there is a winning next state, we can ignore undecided
+                        // next states and mark the current state as also winning.
+                        states[id].expected_outcome = winning_outcome;
+                    }
+                    else if (has_undecided_next_state)
+                    {
+                        // Otherwise, we must solve all next states before solving the
+                        // current state.
+                        states[id].expected_outcome = State.Outcome.Undecided;
+                    }
+                    else if (has_draw_next_state)
+                    {
+                        // If the current state is not winning, at least we want to
+                        // force a draw.
+                        states[id].expected_outcome = State.Outcome.Draw;
+                    }
+                    else
+                    {
+                        // If all else fails, admit defeat.
+                        states[id].expected_outcome = losing_outcome;
+                    }
+                }
+                if (states[id].expected_outcome == State.Outcome.Undecided)
+                {
+                    // If we cannot solve this state, possibly due to unsolved next states,
+                    // try again later.
+                    queue.Enqueue(id);
+                }
+                else
+                {
+                    // If the state is solved, add all unsolved previous states to the queue
+                    // so we can try to solve them later.
+                    foreach (int prev_id in states[id].prev_states)
+                    {
+                        if (states[prev_id].expected_outcome == State.Outcome.Undecided)
+                        {
+                            queue.Enqueue(prev_id);
+                        }
+                    }
+                }
+            }
+
+            // Display.
+            game_tree.Items.Clear();
+            game_tree.Items.Add(states[0]);
         }
     }
 }
--- TicTacToeSolver/TicTacToeGuiSolver/State.cs
+++ TicTacToeSolver/TicTacToeGuiSolver/State.cs
@@ -6,21 +6,165 @@ using System.Text;
 
 namespace TicTacToeGuiSolver
 {
+    // Refer to Program.cs in the other project
+    // for the maths in this class.
     public class State
     {
-        public State()
+        public State(int id)
         {
-            NextStates = new List<State>();
+            this.id = id;
+            prev_states = new List<int>();
+            next_states = new List<int>();
+            expected_outcome = Outcome.Undecided;
+            Initialize();
         }
-        public int ID { get; set; }
+        public int id;
+        // Inclusive.
+        public const int max_board_id = 2 * (6561 + 2187 + 729 + 243 + 81 + 27 + 9 + 3 + 1);
+        // Inclusive.
+        public const int max_id = max_board_id * 2 + 1;
+
+        public List<int> prev_states;
+        public List<int> next_states;
         public List<State> NextStates { get; set; }
+
+        private void Initialize()
+        {
+            // Decode ID.
+            next_player = (NextPlayer)(id % 2);
+            int board_state = id / 2;
+            cells = new CellContent[9];
+            for (int i = 8; i >= 0; i--)
+            {
+                cells[i] = (CellContent)(board_state % 3);
+                board_state /= 3;
+            }
+
+            // Is there an outcome?
+            outcome = Outcome.Undecided;
+            int[] lines = { 12, 345, 678, 36, 147, 258, 48, 246 };
+            foreach (int line in lines)
+            {
+                int pos_0 = line / 100;
+                int pos_1 = (line % 100) / 10;
+                int pos_2 = line % 10;
+                if (cells[pos_0] == CellContent.O && cells[pos_1] == CellContent.O && cells[pos_2] == CellContent.O)
+                {
+                    outcome = Outcome.OWins;
+                    break;
+                }
+                if (cells[pos_0] == CellContent.X && cells[pos_1] == CellContent.X && cells[pos_2] == CellContent.X)
+                {
+                    outcome = Outcome.XWins;
+                    break;
+                }
+            }
+            if (outcome == Outcome.Undecided)
+            {
+                bool any_cell_empty = false;
+                foreach (CellContent c in cells)
+                {
+                    if (c == CellContent.Empty)
+                    {
+                        any_cell_empty = true;
+                        break;
+                    }
+                }
+                if (!any_cell_empty)
+                {
+                    outcome = Outcome.Draw;
+                }
+            }
+
+            // PS. Some boards are impossible, such as ones with multiple lines of Os and Xs.
+            // However, boards with an outcome have no next states, so these impossible
+            // states should be isolated from the game tree.
+            // Thus there's no need to handle them.
+
+            // If no outcome, find next states.
+            // Previous states will be filled by the Solver class.
+            if (outcome != Outcome.Undecided)
+            {
+                expected_outcome = outcome;
+                return;
+            }
+            CellContent next_cell = (next_player == NextPlayer.O) ? CellContent.O : CellContent.X;
+            NextPlayer next_next_player = (next_player == NextPlayer.O) ? NextPlayer.X : NextPlayer.O;
+            board_state = id / 2;
+            int delta_state = 6561;
+            for (int i = 0; i < 9; i++)
+            {
+                if (cells[i] == CellContent.Empty)
+                {
+                    int next_board = board_state + (int)next_cell * delta_state;
+                    next_states.Add(next_board * 2 + (int)next_next_player);
+                }
+                delta_state /= 3;
+            }
+        }
+
+        public enum Outcome
+        {
+            Undecided = -1,
+            OWins = 0,
+            Draw = 1,
+            XWins = 2
+        }
+        public Outcome outcome;
+        // This is the expected outcome from the current state, assuming
+        // both players play best moves.
+        public Outcome expected_outcome;
+
+        // Below are decoded from ID. We have enough memory to store them so why not.
+
+        public enum CellContent
+        {
+            Empty = 0,
+            O = 1,
+            X = 2
+        }
+        public CellContent[] cells;
+        public enum NextPlayer
+        {
+            O = 0,
+            X = 1
+        }
+        public NextPlayer next_player;
 
         public string Display
         {
             get
             {
-                return "State #" + ID.ToString() + "\n" + "Another line";
+                string display = "";
+
+                // Line 1: row 1, state #
+                for (int i = 0; i < 3; i++) display += DisplayCell(i);
+                display += $"  #{id}\n";
+
+                // Line 2: row 2, next player
+                for (int i = 3; i < 6; i++) display += DisplayCell(i);
+                display += $"  Next player: {next_player}\n";
+
+                // Line 3: row 3, expected outcome
+                for (int i = 6; i < 9; i++) display += DisplayCell(i);
+                display += $"  Expected outcome: {expected_outcome}\n";
+
+                return display;
             }
+        }
+
+        private string DisplayCell(int i)
+        {
+            switch (cells[i])
+            {
+                case CellContent.Empty:
+                    return "-";
+                case CellContent.O:
+                    return "O";
+                case CellContent.X:
+                    return "X";
+            }
+            return "";
         }
     }
 }
